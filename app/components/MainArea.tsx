cacffb8461390f03f185173f10458458706e169f
error707-persona <areeshasayed09@gmail.com>
2024-11-21 22:16:33 +0530

    project setup

---
+++ app/components/MainArea.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Tabs from './Tabs'
+import TabContent from './TabContent'
+
+const MainArea = () => {
+    return (
+        <div>
+            <Tabs />
+            <TabContent />
+        </div>
+    )
+}
+
+export default MainArea
